9eb1b1ac1bd859763b2197a659c107da321eda5e
flynn <crf204@gmail.com>
2019-04-16 00:22:33 -0400

    refactor and reorganize system commands

--- soh/cli.py
+++ soh/cli.py
@@ -6,8 +6,19 @@ import click
 
 from soh.b64 import b64
 from soh.epoch import epoch
-from soh.sys import sys
+from soh.system import arch
+from soh.system import cores
+from soh.system import eip
+from soh.system import ip
+from soh.system import mac
+from soh.system import machine
+from soh.system import node
+from soh.system import proc
+from soh.system import sys
+from soh.system import sysver
 from soh.uuid import uuid_
+from soh.util import clipboard_output
+from soh.__version__ import __version__
 
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -19,10 +30,27 @@ def cli():
     pass  # pragma: no cover
 
 
+@click.command()
+@clipboard_output
+def version():
+    """Sleight of hand (soh) version."""
+    return __version__
+
+
 cli.add_command(b64, name="b64")
 cli.add_command(epoch, name="epoch")
+cli.add_command(arch, name="arch")
+cli.add_command(cores, name="cores")
+cli.add_command(eip, name="eip")
+cli.add_command(ip, name="ip")
+cli.add_command(mac, name="mac")
+cli.add_command(machine, name="machine")
+cli.add_command(node, name="node")
+cli.add_command(proc, name="proc")
 cli.add_command(sys, name="sys")
+cli.add_command(sysver, name="sysver")
 cli.add_command(uuid_, name="uuid")
+cli.add_command(version, name="version")
 
 
 if __name__ == "__main__":
---
+++ soh/system.py
@@ -0,0 +1,97 @@
+import multiprocessing
+import platform
+import socket
+from uuid import getnode
+
+import click
+import requests
+
+from soh.util import clipboard_output
+
+
+@click.command(short_help="OS version")
+@clipboard_output
+def arch():
+    """Platform architecture."""
+    return "; ".join(platform.architecture())
+
+
+@click.command(short_help="Number of cores")
+@clipboard_output
+def cores():
+    """Number of cores."""
+    return str(multiprocessing.cpu_count())
+
+
+@click.command(short_help="External IP address")
+@clipboard_output
+def eip():
+    """External IP address."""
+    response = requests.get("https://api6.ipify.org?format=json")
+    try:
+        response.raise_for_status()
+    # TODO cover this
+    except requests.RequestException:  # pragma: no cover
+        raise click.ClickException("External request failed.")
+
+    value = response.json()["ip"]
+
+    return value
+
+
+@click.command(short_help="Local IP address")
+@clipboard_output
+def ip():
+    """Local IP address."""
+    value = socket.gethostbyname(socket.gethostname())
+    return str(value)
+
+
+@click.command(short_help="Local MAC address")
+@click.option("-u", "--upper", is_flag=True, help="use upper case")
+@clipboard_output
+def mac(upper):
+    """MAC address."""
+    node = getnode()
+    value = ":".join(("%012x" % node)[i : i + 2] for i in range(0, 12, 2))
+
+    if upper:
+        value = value.upper()
+
+    return str(value)
+
+
+@click.command(short_help="Machine information")
+@clipboard_output
+def machine():
+    """Machine type."""
+    return platform.machine()
+
+
+@click.command(short_help="OS version")
+@clipboard_output
+def node():
+    """Node name."""
+    return platform.node()
+
+
+@click.command(short_help="Processor information")
+@clipboard_output
+def proc():
+    """Processor."""
+    return platform.processor()
+
+
+# TODO cover this
+@click.command(short_help="System information")
+@clipboard_output
+def sys():  # pragma: no cover
+    """System information."""
+    return platform.system()
+
+
+@click.command(short_help="OS version")
+@clipboard_output
+def sysver():
+    """System release version."""
+    return platform.version()
