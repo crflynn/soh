7105292780ffd04c7ccb4d1c2d9c801bf15028f3
flynn <crf204@gmail.com>
2019-04-17 19:57:17 -0400

    create files command

--- soh/cli.py
+++ soh/cli.py
@@ -6,6 +6,8 @@ Entry point: $ soh version
 import click
 
 from soh.b64 import b64
+from soh.create import create
+from soh.create import gitignore
 from soh.epoch import epoch
 from soh.system import arch
 from soh.system import cores
@@ -40,6 +42,8 @@ def version():
 
 
 cli.add_command(b64, name="b64")
+cli.add_command(create, name="create")
+cli.add_command(gitignore, name="gitignore")
 cli.add_command(epoch, name="epoch")
 cli.add_command(arch, name="arch")
 cli.add_command(cores, name="cores")
