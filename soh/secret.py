21ab1b60250e6f68d4b9db85fe55487d7c3144ef
flynn <crf204@gmail.com>
2019-04-20 12:38:50 -0400

    add secrets; add hide functionality to clipboard decorator

---
+++ soh/secret.py
@@ -0,0 +1,77 @@
+"""Secrets generation CLI functionality.
+
+Entry point: $ soh secret b [OPTS]
+Entry point: $ soh secret h [OPTS]
+Entry point: $ soh secret u [OPTS]
+Entry point: $ soh secret pw [OPTS]
+"""
+import random
+import secrets
+import string
+
+import click
+
+from soh.util import clipboard_output
+
+
+@click.group(invoke_without_command=False, short_help="Base64 operations")
+def secret():
+    """Secrets generation."""
+    pass  # pragma: no cover
+
+
+@secret.command(short_help="Generate a urlsafe token")
+@click.option("-b", "--bytes", "nbytes", default=32, show_default=True, help="number of bytes")
+@clipboard_output
+def u(nbytes):
+    """Generate a urlsafe secret of n bytes."""
+    return secrets.token_urlsafe(nbytes)
+
+
+@secret.command(short_help="Generate a hex token")
+@click.option("-b", "--bytes", "nbytes", default=32, show_default=True, help="number of bytes")
+@clipboard_output
+def h(nbytes):
+    """Generate a urlsafe secret of n bytes."""
+    return secrets.token_hex(nbytes)
+
+
+@secret.command(short_help="Generate a bytes token")
+@click.option("-b", "--bytes", "nbytes", default=32, show_default=True, help="number of bytes")
+@clipboard_output
+def b(nbytes):
+    """Generate a urlsafe secret of n bytes."""
+    return str(secrets.token_bytes(nbytes))[2:-1]
+
+
+@secret.command(short_help="Generate a password")
+@click.option("-n", default=8, show_default=True, help="quantity of numbers")
+@click.option("-s", default=4, show_default=True, help="quantity of symbols")
+@click.option("-l", default=32, show_default=True, help="length")
+@click.option("-a", is_flag=True, default=False, show_default=True, help="avoid ambiguous")
+@click.option("-p", "--protect", "protect", is_flag=True, default=False, show_default=True, help="hide from console")
+@clipboard_output
+def pw(n, s, l, a):
+    """Generate a password."""
+    if n + s > 32:
+        raise click.ClickException("Password length must exceed character requirements.")
+
+    ambiguous = "Iil1Lo0O"
+
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
+
+    # avoid ambiguous characters
+    if a:
+        letters = letters.strip(ambiguous)
+        numbers = numbers.strip(ambiguous)
+        symbols = symbols.strip(ambiguous)
+
+    password = "".join(secrets.choice(numbers) for _ in range(n))
+    password += "".join(secrets.choice(symbols) for _ in range(s))
+    password += "".join(secrets.choice(letters) for _ in range(l - n - s))
+
+    password = list(password)
+    random.shuffle(password)
+    return "".join(password)
