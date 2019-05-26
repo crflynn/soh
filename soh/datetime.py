43ebb943919825d36c28c83111d5ffc6a0a667f6
Flynn <crf204@gmail.com>
2019-05-26 16:21:05 -0400

    use arrow; add dt from

--- soh/datetime.py
+++ soh/datetime.py
@@ -2,38 +2,70 @@
 
 Entry point: $ soh utcnow
 """
-import datetime
-
+import arrow
+from arrow.parser import ParserError
+from dateutil.zoneinfo import get_zonefile_instance
 import click
 
 from soh.util import clipboard_output
 
 
-# TODO utc vs local
-# TODO timezones
+@click.group(invoke_without_command=False, short_help="+ Datetimes")
+def dt():
+    """Base64 decoding and encoding."""
+    pass  # pragma: no cover
 
 
-@click.command(short_help="UTC now")
-@click.option("-t", is_flag=True, default=False, help="use T separator")
-@clipboard_output
-def now(t):
-    """Current datetime."""
-    if t:
-        return datetime.datetime.utcnow().isoformat("T")
+def get_tz_time(time_, tz):
+    if tz == "list":
+        return "\n".join(sorted(list(get_zonefile_instance().zones)))
+    if tz is not None:
+        try:
+            the_time = time_.to(tz)
+        except ParserError:
+            raise click.ClickException("Unknown timezone: " + tz)
     else:
-        return str(datetime.datetime.utcnow())
+        the_time = time_
+    return the_time
 
 
-@click.command(short_help="UTC time")
+@dt.command(short_help="UTC now")
+@click.option("-tz", show_default=True, help="timezone")
 @clipboard_output
-def time():
+def now(tz):
+    """Current datetime."""
+    return str(get_tz_time(arrow.utcnow(), tz))
+
+
+@dt.command(short_help="UTC time")
+@click.option("-tz", show_default=True, help="timezone")
+@clipboard_output
+def time(tz):
     """Current time."""
-    return str(datetime.datetime.utcnow())[11:]
+    if tz == "list":
+        return str(get_tz_time(arrow.utcnow(), tz))
+    else:
+        return str(get_tz_time(arrow.utcnow(), tz))[11:]
 
 
-# TODO use T sep
-@click.command(short_help="UTC date")
+@dt.command(short_help="UTC date")
+@click.option("-tz", show_default=True, help="timezone")
 @clipboard_output
-def today():
+def date(tz):
     """Current date."""
-    return str(datetime.date.today())
+    if tz == "list":
+        return str(get_tz_time(arrow.utcnow(), tz))
+    else:
+        return str(get_tz_time(arrow.utcnow(), tz).date())
+
+
+@dt.command(name="from", short_help="Datetime from epoch seconds")
+@click.option("-tz", show_default=True, help="timezone")
+@click.argument("epoch")
+@clipboard_output
+def from_(tz, epoch):
+    """Datetime from epoch seconds."""
+    if tz == "list":
+        return str(get_tz_time(arrow.get(epoch), tz))
+    else:
+        return str(get_tz_time(arrow.get(epoch), tz))
--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,68 +1,133 @@
 import time
 
 from click.testing import CliRunner
+from dateutil.zoneinfo import get_zonefile_instance
 from freezegun import freeze_time
 
 from soh.util import COPIED_TO_CLIPBOARD_MESSAGE
 from soh.datetime import time
 from soh.datetime import now
-from soh.datetime import today
+from soh.datetime import date
+from soh.datetime import from_
 
 
-FROZEN_TIME = "2019-06-09 04:20:00.123456"
+FROZEN_TIME = "2019-06-09T04:20:00.123456+00:00"
+FROZEN_TIME_SECONDS = "2019-06-09T04:20:00+00:00"
+FROZEN_TIME_EPOCH = "1560054000"
+FROZEN_TIME_EST = "2019-06-08T23:20:00.123456-05:00"
+FROZEN_TIME_EST_SECONDS = "2019-06-08T23:20:00-05:00"
+
+
+TIMEZONES = sorted(list(get_zonefile_instance().zones))
 
 
 @freeze_time(FROZEN_TIME)
-def test_now(t):
+def test_now(tz_label, tz):
     runner = CliRunner()
 
     # build args
     args = []
-    if t is not None:
-        args += [t]
-    # if clip is not None:
-    #     args += [clip]
+    if tz_label is not None:
+        args += [tz_label, tz]
 
     result = runner.invoke(now, args)
 
-    assert result.exit_code == 0
-
     output = result.output.rsplit(COPIED_TO_CLIPBOARD_MESSAGE)[0].replace("\n", "")
-    if t is not None:
-        assert output == FROZEN_TIME.replace(" ", "T")
-    else:
+    if tz_label is not None:
+        if tz == "list":
+            assert result.exit_code == 0
+            assert "EST" in output
+        elif tz == "EST":
+            assert result.exit_code == 0
+            assert output == FROZEN_TIME_EST
+        elif tz in TIMEZONES:
+            assert result.exit_code == 0
+        else:
+            assert result.exit_code != 0
+    elif tz_label is None:
+        assert result.exit_code == 0
         assert output == FROZEN_TIME
 
 
 @freeze_time(FROZEN_TIME)
-def test_today():
+def test_today(tz_label, tz):
     runner = CliRunner()
 
     # build args
     args = []
-    # if clip is not None:
-    #     args += [clip]
+    if tz_label is not None:
+        args += [tz_label, tz]
 
-    result = runner.invoke(today, args)
-
-    assert result.exit_code == 0
+    result = runner.invoke(date, args)
 
     output = result.output.rsplit(COPIED_TO_CLIPBOARD_MESSAGE)[0].replace("\n", "")
-    assert output == FROZEN_TIME[:10]
+    if tz_label is not None:
+        if tz == "list":
+            assert result.exit_code == 0
+            assert "EST" in output
+        elif tz == "EST":
+            assert result.exit_code == 0
+            assert output == FROZEN_TIME_EST[:10]
+        elif tz in TIMEZONES:
+            assert result.exit_code == 0
+        else:
+            assert result.exit_code != 0
+    elif tz_label is None:
+        assert result.exit_code == 0
+        assert output == FROZEN_TIME[:10]
 
 
 @freeze_time(FROZEN_TIME)
-def test_time():
+def test_time(tz_label, tz):
     runner = CliRunner()
 
     # build args
     args = []
-    # if clip is not None:
-    #     args += [clip]
+    if tz_label is not None:
+        args += [tz_label, tz]
 
     result = runner.invoke(time, args)
 
-    assert result.exit_code == 0
+    output = result.output.rsplit(COPIED_TO_CLIPBOARD_MESSAGE)[0].replace("\n", "")
+    if tz_label is not None:
+        if tz == "list":
+            assert result.exit_code == 0
+            assert "EST" in output
+        elif tz == "EST":
+            assert result.exit_code == 0
+            assert output == FROZEN_TIME_EST[11:]
+        elif tz in TIMEZONES:
+            assert result.exit_code == 0
+        else:
+            assert result.exit_code != 0
+    elif tz_label is None:
+        assert result.exit_code == 0
+        assert output == FROZEN_TIME[11:]
+
+
+def test_from(tz_label, tz):
+    runner = CliRunner()
+
+    # build args
+    args = [FROZEN_TIME_EPOCH]
+    if tz_label is not None:
+        args += [tz_label, tz]
+
+    result = runner.invoke(from_, args)
 
     output = result.output.rsplit(COPIED_TO_CLIPBOARD_MESSAGE)[0].replace("\n", "")
-    assert output == FROZEN_TIME[11:]
+    if tz_label is not None:
+        if tz == "list":
+            assert result.exit_code == 0
+            assert "EST" in output
+        elif tz == "EST":
+            print(args)
+            assert result.exit_code == 0
+            assert output == FROZEN_TIME_EST_SECONDS
+        elif tz in TIMEZONES:
+            assert result.exit_code == 0
+        else:
+            assert result.exit_code != 0
+    elif tz_label is None:
+        assert result.exit_code == 0
+        assert output == FROZEN_TIME_SECONDS
