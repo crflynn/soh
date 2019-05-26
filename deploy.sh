b8ef18fab7962aa2c8cf5561f88525e9a4a4de65
Flynn <crf204@gmail.com>
2019-05-26 18:24:24 -0400

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export VERSION=$(poetry run soh version)
 python setup.py sdist bdist_wheel
-twine upload dist/soh-$(poetry run soh version)*
+twine upload dist/soh-${VERSION}*
+git tag -a ${VERSION} -m "${VERSION}"
+git push --tags
