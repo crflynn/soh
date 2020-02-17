2bd1becf74c1feed746292b77240fc787a323239
flynn <crf204@gmail.com>
2020-02-16 19:18:27 -0500

    fmt

--- src/create.rs
+++ src/create.rs
@@ -1,10 +1,10 @@
 use crate::error::BoxResult;
 use clap::ArgMatches;
-use std::convert::TryFrom;
-use std::collections::BTreeMap;
 use reqwest;
-use serde::{Deserialize, Serialize};
 use reqwest::header::USER_AGENT;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
@@ -12,7 +12,8 @@ use std::path::Path;
 const GITIGNORE_FILE: &str = ".gitignore";
 const GITIGNORE_URL: &str = "https://api.github.com/repos/github/gitignore/contents";
 const LICENSE_FILE: &str = "LICENSE.txt";
-const LICENSE_URL: &str = "https://api.github.com/repos/github/choosealicense.com/contents/_licenses";
+const LICENSE_URL: &str =
+    "https://api.github.com/repos/github/choosealicense.com/contents/_licenses";
 const SOH_USER_AGENT: &str = "soh/0.2.0";
 
 // deserialize JSON responses into this struct
@@ -40,13 +41,16 @@ pub fn github_download(matches: &ArgMatches, filename: &str, url: &str) -> BoxRe
     let object_type = matches.value_of("INPUT").unwrap();
 
     if object_type != "list" && !overwrite && Path::new(filename).exists() {
-        return Err(Box::try_from("File already exists; aborting. Use -o to overwrite file.").unwrap())
+        return Err(
+            Box::try_from("File already exists; aborting. Use -o to overwrite file.").unwrap(),
+        );
     }
 
     let client = reqwest::blocking::Client::new();
     let result = client.get(url).header(USER_AGENT, SOH_USER_AGENT).send();
 
-    let response: Vec<GithubContentsEntry> = result.unwrap().json::<Vec<GithubContentsEntry>>().unwrap();
+    let response: Vec<GithubContentsEntry> =
+        result.unwrap().json::<Vec<GithubContentsEntry>>().unwrap();
 
     // use a btree to maintain the order
     let mut files: BTreeMap<String, String> = BTreeMap::new();
@@ -54,24 +58,35 @@ pub fn github_download(matches: &ArgMatches, filename: &str, url: &str) -> BoxRe
     // pack lower-case names, file download urls into map
     for entry in response.iter() {
         let l: Vec<&str> = entry.name.split('.').collect();
-        if l.len() > 1 && l[0] != ""{
-            files.insert(l[0].to_lowercase(), entry.download_url.as_ref().unwrap().to_string());
+        if l.len() > 1 && l[0] != "" {
+            files.insert(
+                l[0].to_lowercase(),
+                entry.download_url.as_ref().unwrap().to_string(),
+            );
         }
     }
 
     let lower_name = object_type.to_lowercase();
     match lower_name.as_str() {
-        "list" =>  {
+        "list" => {
             let keys: Vec<String> = files.keys().cloned().collect();
             Ok(keys.join("\n"))
         }
         v => {
             // ensure that the entity of interest is in the list of entities
-            let download_url = match files.get(v){
+            let download_url = match files.get(v) {
                 Some(v) => v,
-                None => return Err(Box::try_from("Must supply a valid value. Run with `list` to see valid values").unwrap())
+                None => {
+                    return Err(Box::try_from(
+                        "Must supply a valid value. Run with `list` to see valid values",
+                    )
+                    .unwrap())
+                }
             };
-            let file_response = client.get(download_url).header(USER_AGENT, SOH_USER_AGENT).send();
+            let file_response = client
+                .get(download_url)
+                .header(USER_AGENT, SOH_USER_AGENT)
+                .send();
             let mut contents: String = file_response.unwrap().text().unwrap();
             // license files have additional frontmatter that needs to be stripped away
             if filename == LICENSE_FILE {
